ec58d78e3193bda7219ceabf4e0fa1bf4013cd20
lxq0716 <161689619+lxq0716@users.noreply.github.com>
2024-06-26 21:22:07 +0800

    Add files via upload

---
+++ 期末大作业/ToDoList/myvue3/myvue3/src/components/AddTodo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <label>编号</label><input type="text" max="10" v-model.trim="todoId" />
+        <br />
+        <label>任务名称</label><input type="text" maxlength="50" v-model.trim="todoElement" />
+        <br />
+        <button @click="submintodo">保存</button>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const todoElement = ref('')
+const todoId = ref('')
+
+//注册点击事件
+const emit = defineEmits(['add-todo'])
+
+//保存函数的编写
+const submintodo = () => {
+    if (todoElement.value) {
+        console.log(todoElement.value)
+        emit('add-todo', todoElement.value, todoId.value)
+        todoElement.value = ' '
+        todoId.value = ' '
+    }
+}
+</script>
